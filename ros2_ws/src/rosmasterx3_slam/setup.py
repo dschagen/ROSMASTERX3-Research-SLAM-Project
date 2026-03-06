355d0df6024bbd8804c5410ed67bd100597069fb
jetson <dschagen6@gmail.com>
2026-03-06 13:42:11 -0500

    Add working scan_monitor node and verify odom imu bringup

--- ros2_ws/src/rosmasterx3_slam/setup.py
+++ ros2_ws/src/rosmasterx3_slam/setup.py
@@ -1,21 +1,26 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 package_name = 'rosmasterx3_slam'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        (
+            'share/ament_index/resource_index/packages',
+            ['resource/' + package_name],
+        ),
+        (
+            'share/' + package_name,
+            ['package.xml'],
+        ),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='root',
-    maintainer_email='1461190907@qq.com',
-    description='TODO: Package description',
+    maintainer='Daniel Schagen',
+    maintainer_email='your_email@example.com',
+    description='ROSMASTERX3 SLAM research package',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
