423e85dff320807de8c7a6262cf4e7345a9e56be
dschagen <103970867+dschagen@users.noreply.github.com>
2026-03-06 13:07:25 -0500

    Update setup.py
    Updated setup.py to include the new scan_monitor node

--- ros2_ws/src/rosmasterx3_slam/setup.py
+++ ros2_ws/src/rosmasterx3_slam/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'scan_monitor = rosmasterx3_slam.nodes.scan_monitor:main',
         ],
     },
 )
